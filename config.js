8eacddacdfe81f79581a6215506c5339fe362c78
weekly-sa <alqwqndybas@gmail.com>
2025-11-09 22:34:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://bhnoauukllcldacmdxzf.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJobm9hdXVrbGxjbGRhY21keHpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3MTQwNTUsImV4cCI6MjA3ODI5MDA1NX0.r-wmv7QY1aTWuzXCPftZHs9dqKqDmmZ9bds3KH0FDbE';
+const SUPABASE_URL = 'https://ugbhzggjxvsfsxxzlcaq.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnYmh6Z2dqeHZzZnN4eHpsY2FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NTk3NTAsImV4cCI6MjA3ODAzNTc1MH0.m6ht8GEZxVAQM1lKPnWWO6QQsaJgGrPYROpjQnk6RGA';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
