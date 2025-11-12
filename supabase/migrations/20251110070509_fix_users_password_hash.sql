25e5c9047faacefe114f199c9b63f91cf5d92c1f
weekly-sa <alqwqndybas@gmail.com>
2025-11-12 10:51:15 +0300

    Delete supabase/migrations directory

--- supabase/migrations/20251110070509_fix_users_password_hash.sql
+++
@@ -1,8 +0,0 @@
-/*
-  # Fix users table password_hash column
-
-  Make password_hash nullable since we use Supabase Auth, not local password storage
-*/
-
-ALTER TABLE users ALTER COLUMN password_hash DROP NOT NULL;
-ALTER TABLE users ALTER COLUMN password_hash SET DEFAULT '';
